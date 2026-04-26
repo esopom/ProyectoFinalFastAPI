97b023427235ad48af2f8ad67d22f5f7619f2ef0
AdrianTomas <tomasadrian876@gmail.com>
2026-04-26 19:50:22 +0200

    Refactor Portatil model to use TipoOS enum for OS field instead of string

--- api/utilidades/models.py
+++ api/utilidades/models.py
@@ -3,13 +3,19 @@ from typing import Union
 from enum import Enum
 from pydantic import BaseModel, Field
 
+# Control Enum OS
+class TipoOS(str, Enum):
+    mac = "mac"
+    windows = "windows"
 
 # Modelo para un Laptop
 class Portatil(BaseModel):
     modelo: str
     precio : int = 0
-    OS : str = None
+    OS : TipoOS
     marcagpu : str =  None
     memoriaram : int = 0
 
+
+    
 
